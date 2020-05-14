769c594595b8a76e51a6cd86e3c8d5d171570418
OkoyePeter <Okoyep98@gmail.com>
2020-05-14 15:54:02 +0100

    done

---
+++ captcha.php
@@ -0,0 +1,48 @@
+<?php
+    session_start();
+
+    function generateCaptchaImage($text = 'abc')
+    {
+        header("Content-Type: image/png");
+
+        $width = 200;
+        $height = 30;
+        // create the image
+        $im = imagecreatetruecolor($width, $height);
+
+        // create some color
+        $white = imagecolorallocate($im, 255, 255, 255);
+        $grey = imagecolorallocate($im, 128, 128, 128);
+        $black = imagecolorallocate($im, 0, 65, 123);
+        imagefilledrectangle($im, 0, 0, 399, 29, $white);
+
+        // ADD NOISE, Draw background squares
+        $square_count = 6;
+        for ($i=0; $i < $square_count; $i++) { 
+            $cx = rand(0, $width);
+            $cy = (int)rand(0, $height/2);
+            $h = $cy + (int)rand(0, $height/5);
+            $w = $cx + (int)rand($width/3, $width);
+            imagefilledrectangle($im, $cx, $cy, $w, $h, $white);
+        }
+
+        // ADD NOISE, Draw ellipse
+        $ellipse_count = 10;
+        for ($i=0; $i < $ellipse_count; $i++) { 
+            # code...
+            $cx = (int)rand(-1*($width/2), $width+($width/2));
+            $cy = (int)rand(-1*($height/2), $height+($height/2));
+            $h = (int)rand($height/2, 2*$height);
+            $w = (int)rand($width/2, 2*$width);
+            imageellipse($im, $cx, $cy, $w, $h, $grey);
+        }
+
+        $font = dirname(__FILE__). '/fonts/ThisisKeSha.ttf';
+        imagettftext($im, 20, 0, 11, 21, $grey, $font, $text); //add some shadow to text
+        imagettftext($im, 20, 0, 10, 20, $black, $font, $text); //add the text
+
+        imagepng($im);
+        imagedestroy($im);
+    }
+    generateCaptchaImage($_SESSION['captcha']);
+?>
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-    session_start();
-    
+    session_start(); 
     function generateRandomString($length = 8)
     {
         $character = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -11,51 +10,83 @@
         }
         return $randomString;
     }
-
-    function generateCaptchaImage($text = 'abc')
-    {
-        header("Content-Type: image/png");
-
-        $width = 200;
-        $height = 30;
-        // create the image
-        $im = imagecreatetruecolor($width, $height);
-
-        // create some color
-        $white = imagecolorallocate($im, 255, 255, 255);
-        $grey = imagecolorallocate($im, 128, 128, 128);
-        $black = imagecolorallocate($im, 0, 65, 123);
-        imagefilledrectangle($im, 0, 0, 399, 29, $white);
-
-        // ADD NOISE, Draw background squares
-        $square_count = 6;
-        for ($i=0; $i < $square_count; $i++) { 
-            $cx = rand(0, $width);
-            $cy = (int)rand(0, $height/2);
-            $h = $cy + (int)rand(0, $height/5);
-            $w = $cx + (int)rand($width/3, $width);
-            imagefilledrectangle($im, $cx, $cy, $w, $h, $white);
-        }
-
-        // ADD NOISE, Draw ellipse
-        $ellipse_count = 10;
-        for ($i=0; $i < $ellipse_count; $i++) { 
-            # code...
-            $cx = (int)rand(-1*($width/2), $width+($width/2));
-            $cy = (int)rand(-1*($height/2), $height+($height/2));
-            $h = (int)rand($height/2, 2*$height);
-            $w = (int)rand($width/2, 2*$width);
-            imageellipse($im, $cx, $cy, $w, $h, $grey);
-        }
-
-        $font = dirname(__FILE__). '/fonts/ThisisKeSha.ttf';
-        imagettftext($im, 20, 0, 11, 21, $grey, $font, $text); //add some shadow to text
-        imagettftext($im, 20, 0, 10, 20, $black, $font, $text); //add the text
-
-        imagepng($im);
-        imagedestroy($im);
-    }
-
-    $_SESSION['captcha'] = generateRandomString();
-    generateCaptchaImage($_SESSION['captcha']);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>captcha</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+        .answerFace{
+            width: 13em;
+            height: 13em;
+        }
+
+        .answer{
+            display: grid;
+            grid-gap: 10px;
+            grid-template: auto auto;
+            grid-auto-flow: row; 
+            justify-items: center;
+        }
+    </style>
+</head>
+<body>  
+    <div class="container mt-4">
+        <?php 
+            if (!isset($_POST['captcha'])) {
+                $_SESSION['captcha'] = generateRandomString();
+            } else {
+                if ($_SESSION['captcha'] === $_POST['captcha']) {?>
+                    <div class="row answer">
+                        <img src="https://www.askideas.com/media/07/Cute-Happy-Baby-Picture.jpg" class="answerFace thumbnail">
+                        <p class="font-weight-bold text-center d-block">You are a human! Welcome.....</p>
+                    </div>
+                <?php
+                    unset($_POST['captcha']);
+                    $_SESSION['captcha'] = generateRandomString();
+                } else {?>
+                    <div class="row answer">
+                        <img src="https://image.shutterstock.com/image-photo/head-shoot-cute-baby-blue-260nw-88582219.jpg" class="answerFace thumbnail">
+                        <p class="font-weight-bold text-center">Uh You are a bot!!!!!!</p>
+                    </div>
+                <?php
+                    $_SESSION['captcha'] = generateRandomString();
+                }
+                
+            }
+            
+        ?>
+        <div class="row">
+            <div class="col-md-6 offset-3">
+                <div class="panel">
+                    <hr>
+                    <div class="panel-heading">
+                        <h1 class="text-center">
+                            Am you a Human?
+                        </h1>
+                    </div>
+                    <hr>
+                    <center>
+                        <img src="captcha.php" alt="">
+                    </center>
+                    <form action="" method="post" class="mt-5">
+                    <div class="input-group mb-3">
+                        <input name="captcha" type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" placeholder="enter the text you see.....">
+                        <div class="input-group-append">
+                            <button class="btn btn-danger" type="submit" id="button-addon2">Button</button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
